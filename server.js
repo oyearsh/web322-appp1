0fa7d5b8754788d749d18aa2a98e8f4030907c81
Arshdeep Singh <arshbhandal02@gmail.com>
2023-02-05 20:31:09 -0500

    fiiiiinal commit

--- server.js
+++ server.js
@@ -17,65 +17,60 @@ const { initialize, getAllPosts, getPublishedPosts, getCategories } = require(".
 
 const app = express();
 
-// Using the 'public' folder as our static folder
+// public folder as static folder
 app.use(express.static('public')); 
 
 const HTTP_PORT = process.env.PORT || 8080;
 
-// ========== Home Page Route ==========
 app.get("/", (req,res) => {
   res.redirect("/about");
 });
 
-// ========== About Page Route ==========
 app.get("/about", (req, res) => {
     res.sendFile(path.join(__dirname, "views", "about.html"));
 })
 
-// ========== Blog Page Route ==========
 app.get("/blog", (req, res) => {
   getPublishedPosts()
   .then((data) => {
     res.send(data)
   })
-  // Error Handling
+  
   .catch((err) => {
     res.send(err);
   })
 });
 
-// ========== Posts Page Route ==========
 app.get("/posts", (req, res) => {
   getAllPosts()
     .then((data) => {
       res.send(data)
     })
-    // Error Handling
+
     .catch((err) => {
       res.send(err);
     })
 });
 
-// ========== Categories Page Route ==========
+
 app.get("/categories", (req, res) => {
   getCategories()
   .then((data) => {
     res.send(data)
   })
-  // Error Handling
+
   .catch((err) => {
     res.send(err);
   })
 })
 
-// ========== HANDLE 404 REQUESTS ==========
+
 app.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, "views", "notFoundPage.html"));
 })
 
-// ========== Setup http server to listen on HTTP_PORT ==========
+
 initialize().then(() => {
-  // Start the server after the files are read and the initialization is done
   app.listen(HTTP_PORT, () => {
     console.log("Express http server listening on: " + HTTP_PORT);
   });
